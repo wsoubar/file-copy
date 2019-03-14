05ab442ff0c36e8b6046d7db71022816aca3840a
Wagner Barbosa <wsoubar@gmail.com>
2019-03-14 05:25:40 -0300

    testes funcionando

--- src/main/java/br/com/bradseg/coti/jarcopy/Client.java
+++ src/main/java/br/com/bradseg/coti/jarcopy/Client.java
@@ -1,32 +1,48 @@
 package br.com.bradseg.coti.jarcopy;
 
-import java.io.FileOutputStream;
+import java.io.BufferedOutputStream;
+import java.io.DataOutputStream;
+import java.io.File;
 import java.io.IOException;
-import java.io.InputStream;
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
 import java.net.Socket;
+import java.nio.file.Files;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  * Client
  */
 public class Client {
 
-    public static void main(String[] args) throws IOException {
+    private final static Logger LOGGER = Logger.getLogger(Client.class.getName());
 
+    public static void main(String[] args) throws IOException {
+        LOGGER.setLevel(Level.INFO);
+        LOGGER.info("main method");
+        String filename ="C:/dev/java/BSAD-PdfBox-1.2.jar";
+        File file = new File(filename);
         // localhost for testing
-        Socket sock = new Socket("127.0.0.1", 4444);
-        System.out.println("Connecting...");
+        //Socket socket = new Socket("localhost", 4444);
+        Socket socket = new Socket();
+        //socket.setSoTimeout(5000);
+        socket.connect(new InetSocketAddress("localhost", 4445), 5000);
+        LOGGER.info("Connecting...");
 
         // receive file
-        InputStream in = sock.getInputStream();
-        FileOutputStream out = new FileOutputStream(args[0]);
-
-        byte[] mb = new byte[1024];
-
-        for (int c = in.read(mb, 0, 1024); c > 0; c = in.read(mb, 0, 1024)) {
-            out.write(mb, 0, c);
+        //InputStream in = sock.getInputStream();
+        BufferedOutputStream out = new BufferedOutputStream(socket.getOutputStream());
+        try {
+            DataOutputStream d = new DataOutputStream(out);
+            d.writeUTF(filename);
+            Files.copy(file.toPath(), d);
+        } catch (Exception e) {
+            LOGGER.log(Level.SEVERE, e.getMessage(), e);
+        } finally {
+            out.close();
+            socket.close();
+            LOGGER.info("Fim client"); 
         }
-
-        out.close();
-        sock.close();
     }
 }
--- src/main/java/br/com/bradseg/coti/jarcopy/Server.java
+++ src/main/java/br/com/bradseg/coti/jarcopy/Server.java
@@ -1,11 +1,19 @@
 package br.com.bradseg.coti.jarcopy;
 
+import java.io.BufferedInputStream;
+import java.io.DataInputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.nio.file.CopyOption;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  * Server
@@ -35,35 +43,47 @@ public class Server {
 
     private String host;
     private String port;
-    private ServerSocket servsock = null; 
+    private ServerSocket servsock = null;
+    private final static Logger LOGGER = Logger.getLogger(Client.class.getName());
 
-    public Server() {}
+    public Server() {
+        LOGGER.setLevel(Level.INFO);
+    }
+
     public Server(String host, String port) {
         this.host = host;
         this.port = port;
-    } 
+        LOGGER.setLevel(Level.INFO);
+    }
 
     public void startServer() throws IOException {
         // create socket
         servsock = new ServerSocket(Integer.valueOf(this.port));
 
         while (true) {
-            System.out.println("Waiting...");
+            LOGGER.info("Waiting...");
             Socket sock = servsock.accept();
-            System.out.println("Accepted connection : " + sock);
+            LOGGER.info("Accepted connection : " + sock);
 
-            /*
             // sendfile
-            File myFile = new File(args[0]);
-            FileInputStream in = new FileInputStream(myFile);
-            OutputStream out = sock.getOutputStream();
-            System.out.println("Sending...");
-
-            byte[] mb = new byte[1024];
-            for (int c = in.read(mb); c > -1; c = in.read(mb)) {
-                out.write(mb, 0, c);
+            BufferedInputStream in = new BufferedInputStream(sock.getInputStream());
+            DataInputStream d = new DataInputStream(in);
+            try {
+                String origFileName = d.readUTF();
+                File origFile = new File(origFileName);
+                String destFileName = "C:/dev/java/jar-copy/sharedlib/" + origFile.getName();
+                LOGGER.info("--- origFileName ---");
+                LOGGER.info(origFileName);
+                LOGGER.info(Paths.get(origFileName).toString());
+                LOGGER.info("--- destFileName ---");
+                LOGGER.info(destFileName);
+                LOGGER.info(Paths.get(destFileName).toString());
+                Files.copy(d, Paths.get(destFileName), StandardCopyOption.REPLACE_EXISTING);
+                LOGGER.info("arquivo salvo");
+            } catch (Exception e) {
+                LOGGER.log(Level.SEVERE, e.getMessage(), e);
+                d.close();
             }
-            */
-        }        
+        }
     }
 }
