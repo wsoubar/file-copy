bc714afafd3742505b716137f9cf5cc77fdcba06
Wagner Barbosa <wsoubar@gmail.com>
2019-04-29 22:19:09 -0300

    ajustes

--- src/main/resources/server.bat
+++ src/main/resources/server.bat
@@ -1,2 +1,2 @@
 SET JAVA_BIN=.\jre\bin
-%JAVA_BIN%\java -jar ssv-file-copy-1.0.jar -s -porta 17017 -caminho C:/dev/java/jar-copy/sharedlib/ > ssv-file-copy.log
+%JAVA_BIN%\java -jar ssv-file-copy-1.0.jar -s -porta 17017 -caminho e:\sharedlib\ > ssv-file-copy.log
